3f31f75ecc5b83b71bcd34acded0edd01e164965
666vasilyev <vasilyev.jpg@yandex.ru>
2023-09-23 16:57:53 +0300

    add starlette-admin

--- src/app.py
+++ src/app.py
@@ -2,11 +2,17 @@
 import logging
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from fastapi_admin.app import app as admin_app
+from starlette_admin.contrib.sqla import Admin, ModelView
+
+from starlette_admin.views import Link
+from starlette_admin import DropDown
+from src.admin.auth import MyAuthProvider
 from src.core.config import config
 from src.db.session import sessionmanager
 from src.routers import bot_router, channel_router, messages_router, task_router
-
+from starlette.middleware import Middleware
+from starlette.middleware.sessions import SessionMiddleware
+from src.db.models import Bot, ChannelBot
 
 logger = logging.getLogger(__name__)
 
@@ -28,19 +34,37 @@ def get_app(init_db: bool = True):
     @app.on_event("startup")
     async def startup():
         logging.basicConfig(level=logging.INFO)
-        @app.on_event("startup")
-        await admin_app.configure(
-            providers=[
-                LoginProvider(
-                    admin_model=Admin,
-                )
-            ]
-        )
 
     app.include_router(bot_router)
     app.include_router(task_router)
     app.include_router(channel_router)
     app.include_router(messages_router)
 
+    # Create admin
+    admin = Admin(engine=sessionmanager._engine, 
+                  title="Mai Bot Management",
+                  auth_provider=MyAuthProvider(),
+                  middlewares=[Middleware(SessionMiddleware, secret_key=config.SECRET)],)
+
+    # Add view
+    admin.add_view(
+        DropDown(
+            "Bots",
+            views=[
+                ModelView(Bot, label='List'),
+            ],
+        )
+    )
+    admin.add_view(
+        DropDown(
+            "Channels",
+            views=[
+                ModelView(ChannelBot, label='List'),
+            ],
+        )
+    )
+    admin.add_view(Link(label="Swagger API", url="/docs#"))
+    # Mount admin to your app
+    admin.mount_to(app)
 
     return app
