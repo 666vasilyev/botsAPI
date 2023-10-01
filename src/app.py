aae24eef9742270649b0e7ae518cd6afb5694042
666vasilyev <vasilyev.jpg@yandex.ru>
2023-10-01 19:48:24 +0300

    fix cors

--- src/app.py
+++ src/app.py
@@ -31,7 +31,7 @@ def get_app(init_db: bool = True):
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
